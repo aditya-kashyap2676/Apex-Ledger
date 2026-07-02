f71f198ac1966683cb68083172d1d64d0cefeef5
immortaldotjar <arjun.pra1611@gmail.com>
2026-07-02 08:44:47 +0530

    navigation system

--- ApexLedger/src/App.jsx
+++ ApexLedger/src/App.jsx
@@ -4,12 +4,18 @@ import CreateAcc from './components/CreateAcc'
 import Home from './pages/Home'
 import Footer from './components/Footer'
 import Header from './components/Header'
+
+import { Routes,Route,Router } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
 const App = () => {
   return (
     <div>
       {/* <Login/> */}
       <Header/>
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+      </Routes>
       <Footer/>
     </div>
   )
--- ApexLedger/src/components/NavBar.jsx
+++ ApexLedger/src/components/NavBar.jsx
@@ -1,26 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BsLayoutWtf, BsReverseLayoutTextSidebarReverse, BsBarChart, BsInfoCircle } from "react-icons/bs";
-
-const NavBar = () => {
-
+import { FaMoneyBillTrendUp } from 'react-icons/fa6';
+import { ActiveTabContext } from "./Context/Context"
+const NavBar = ({ activeTab, setActiveTab }) => {
     const data = [
         { title: "Dashboard", icon: <BsLayoutWtf /> },
         { title: "Ledger", icon: <BsReverseLayoutTextSidebarReverse /> },
         { title: "Analytics", icon: <BsBarChart /> },
         { title: "About", icon: <BsInfoCircle /> },
-    ]
+    ];
 
     return (
-        <div className = "w-fit h-full">
-            <ul>
-                <li className='center gap-4 font-[Orbitron] font-extrabold md:text-2xl mr-auto  drop-shadow-amber-500'><FaMoneyBillTrendUp className='text-primary-emerald text-4xl border-2 rounded-lg p-1'/> <span className='uppercase drop-shadow-[1px_1px] drop-shadow-primary-emerald'>apex ledger</span></li>
-                {data.map((item,index) => {
-                    return <li key={index}>{item.icon}{item.title}</li>
-                })}
-                <li>Username</li>
+        <div className="w-fit h-screen">
+            <ul className="flex flex-col h-full p-5 bg-secondary-ocean-blue">
+                <li className="center gap-4 font-[Orbitron] font-extrabold md:text-2xl  drop-shadow-amber-500">
+                    <FaMoneyBillTrendUp className="text-white text-4xl border-2 rounded-lg p-1 h-10 w-12" />
+                    <span className="uppercase text-white">
+                        apex ledger
+                    </span>
+                </li>
+                {data.map((item, index) => (
+                    <li
+                        key={index}
+                        onClick={() => setActiveTab(item.title)}
+                        className={`${activeTab === item.title ? " border-2 border-t-0 border-b-0 border-emerald-300 bg-zinc-50/30 text-emerald-300" : "bg-transparent text-white"} flex items-center gap-4 font-sans my-2 p-2 rounded-lg font-bold cursor-pointer`}
+                    >
+                        <span className='text-xl'>{item.icon}</span> {item.title}
+                    </li>
+                ))}
+                <li className="mt-auto">Username</li>
             </ul>
         </div>
-    )
-}
+    );
+};
 
 export default NavBar
